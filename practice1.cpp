303fab5cd07ee70d500680cac11f47b06d1cc7c7
SeWung <49395754+lympsw@users.noreply.github.com>
2022-07-12 18:24:35 +0900

    2022 KAKAO BLIND RECRUITMENT
    신고 결과 받기에 대한 나의 답

--- practice1.cpp
+++ practice1.cpp
@@ -6,13 +6,10 @@ using namespace std;
 
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
     vector<int> answer;
-    vector<int> mid;
-    vector<int>* check1 = new vector<int>[id_list.size()];
-    vector<string> user; vector<string> decl;
+    vector<int>* check1 = new vector<int>[id_list.size()]; //신고한자를 가지고 있기 위해 구현
 
     for (int i = 0; i < id_list.size(); ++i) {
         answer.push_back(0);
-        mid.push_back(0);
     }
     for (int i = 0; i < report.size(); ++i) {
         string a = report[i].substr(0, report[i].find(" "));
@@ -20,17 +17,17 @@ vector<int> solution(vector<string> id_list, vector<string> report, int k) {
         int idx1 = 0; int idx2 = 0;
         for (int j = 0; j < id_list.size(); ++j) {
             if (a == id_list[j]) {
-                idx1 = j;
+                idx1 = j; //신고한자 index
             }
         }
         for (int j = 0; j < id_list.size(); ++j) {
             if (b == id_list[j]) {
-                idx2 = j;
+                idx2 = j; //신고받은자 index
             }
         }
         for (int i = 0; i < check1[idx2].size(); ++i) {
             if (check1[idx2][i] == idx1) {
-                idx1 = -1;
+                idx1 = -1; //똑같은 사람이 똑같은 사람을 신고 했으면 index는 없는 것으로 한다.
             }
         }
         if (idx1 != -1) {
