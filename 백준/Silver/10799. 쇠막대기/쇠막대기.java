196b3a50d1cc0c861d692990ef56e2303a4a2281
SewoongPark <lympsw12@naver.com>
2023-10-14 22:58:32 +0900

    [Silver II] Title: 쇠막대기, Time: 348 ms, Memory: 23068 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -7,22 +7,18 @@ public class Main {
         String str = sc.next();
         Deque<Integer> dq = new ArrayDeque<>();
         int answer = 0;
-        ArrayList<Integer> arr = new ArrayList<>();
+
         for(int i = 0; i < str.length(); ++i){
             if(str.charAt(i) == '(') dq.add(i);
             else {
                 if(dq.isEmpty()) continue;
                 if(dq.getLast() + 1 == i) {
-                    arr.add(i);
                     dq.removeLast();
+                    answer += dq.size();
                 }
                 else {
-                    int s = dq.removeLast();
-                    int e = i;
-                    for(int j = 0; j < arr.size(); ++j){
-                        if(s < arr.get(j) && arr.get(j) < e) ++answer;
-                    }
                     answer += 1;
+                    dq.removeLast();
                 }
             }
         }
