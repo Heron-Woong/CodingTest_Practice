1ad58ec2475004d3a0bc5837fcff411918f29da6
SewoongPark <lympsw12@naver.com>
2024-01-03 14:31:14 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1776 ms, Memory: 384404 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(buf.readLine());
+        int nums[] = new int[10001];
+        for (int i = 0; i < n; i++) {
+            nums[Integer.parseInt(buf.readLine())]++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < 10001; i++) {
+            if(nums[i] != 0) {
+                for (int j = 0; j < nums[i]; j++) {
+                    sb.append(i + "\n");
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
