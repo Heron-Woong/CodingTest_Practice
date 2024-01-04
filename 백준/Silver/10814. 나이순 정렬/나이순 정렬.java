ee2bd61c142bb6265bf299892a37faee9d8c7a80
SewoongPark <lympsw12@naver.com>
2024-01-04 19:53:01 +0900

    [Silver V] Title: 나이순 정렬, Time: 1652 ms, Memory: 63284 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -3,14 +3,12 @@ import java.util.*;
 
 public class Main {
     public static class Member implements Comparable<Member> {
-        String name; int age; int index;
-        public Member(int age, String name, int index) {
+        String name; int age; 
+        public Member(int age, String name) {
             this.name = name;
             this.age = age;
-            this.index = index;
         }
         public int compareTo(Member m) {
-            if(this.age == m.age) return this.index - m.index;
             return this.age - m.age;
         }
     }
@@ -21,7 +19,7 @@ public class Main {
         ArrayList<Member> members = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             String[] member = buf.readLine().split(" ");
-            members.add(new Member(Integer.parseInt(member[0]), member[1], i));
+            members.add(new Member(Integer.parseInt(member[0]), member[1]));
         }
         Collections.sort(members);
         for (int i = 0; i < members.size(); i++) {
